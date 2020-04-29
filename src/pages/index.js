9f600f2b225006921bcbe2b1bbe737f4fe55d522
Mark Beale <mark.beale@sgs.com>
2020-04-29 23:59:57 +0100

    dark toggle working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,10 @@
 import React from "react"
-//import { Link } from "gatsby"
-import { SocialIcon } from "react-social-icons"
+import DarkModeToggle from "./dark-mode-toggle"
+import "./style.css"
 
-import Layout from "../components/layout"
-//import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <SocialIcon url="https://github.com/mabeale" />
-    <SocialIcon url="https://twitter.com/markbealedev" />
-    <SocialIcon url="https://instagram.com/markalanbeale" />
-    <SocialIcon url="https://open.spotify.com/user/mabeale" />
-    <SocialIcon url="https://dev.to/mabeale" />
-    <SocialIcon url="https://www.freecodecamp.org/mabeale" />
-  </Layout>
+export default () => (
+  <div>
+    <DarkModeToggle />
+    <h1>Mark Beale</h1>
+  </div>
 )
-
-export default IndexPage
